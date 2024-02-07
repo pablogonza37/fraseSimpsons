11f8a92b05c44bad4a054a1068f92fa3eb0659b7
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-02-07 10:15:09 -0300

    agregamos el spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,41 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./assets/logosimpson.png";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Spinner } from "react-bootstrap";
 import Frases from "./components/Frases";
 import { useEffect, useState } from "react";
 
 function App() {
   const [personaje, setPersonaje] = useState({});
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
 
   useEffect(() => {
     consultarAPI();
   }, []);
 
   const consultarAPI = async () => {
+    setMostrarSpinner(true);
     const respuesta = await fetch(
       "https://thesimpsonsquoteapi.glitch.me/quotes"
     );
     const datos = await respuesta.json();
     setPersonaje(datos[0]);
+    setMostrarSpinner(false);
   };
+
+  const mostrarComponente = mostrarSpinner ? (
+    <div className="my-4">
+      <Spinner animation="border" variant="dark" />
+    </div>
+  ) : (
+    <Frases personaje={personaje}></Frases>
+  );
 
   return (
     <>
       <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
-        <Frases personaje={personaje}></Frases>
+        {mostrarComponente}
         <Button variant="dark" onClick={consultarAPI}>
           Obtener frase
         </Button>
